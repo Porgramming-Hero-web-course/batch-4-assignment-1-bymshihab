d2f57c86df60a91451a2d0bb5df4fc45392a7d07
bymashihab <khaledshihab221@gmail.com>
2024-11-07 00:06:56 +0600

    first problem solved

---
+++ Assignment-1/src/firstproblem.ts
@@ -0,0 +1,10 @@
+type array = number[];
+function sumArray(nums: array): number {
+  let res: number = 0;
+  nums.map((n) => (res = res + n));
+  console.log(res);
+
+  return res;
+}
+
+sumArray([1, 2]);
