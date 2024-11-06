eec0269091bdab8034b8e49e94b767123d2617c9
bymashihab <khaledshihab221@gmail.com>
2024-11-07 01:11:08 +0600

    problem 2 solved with confussion

---
+++ Assignment-1/src/Problem2.ts
@@ -0,0 +1,19 @@
+// Sample Input:
+// Sample Output:
+// [1, 2, 3, 4, 5]
+
+type NumberArray = number[];
+function removeDuplicates(nums: NumberArray): NumberArray {
+  let newArray: NumberArray = [];
+
+  nums.forEach((n) => {
+    if (!newArray.includes(n)) {
+      newArray.push(n);
+    }
+  });
+  console.log(newArray);
+
+  return newArray;
+}
+
+removeDuplicates([1, 2, 5, 22, 2, 6, 6, 99, 3]);
