6336da8b1bcae3afcd478ebfa3c3ffe22ec72879
bymashihab <khaledshihab221@gmail.com>
2024-11-08 12:50:38 +0600

    changed in problem 8

--- Assignment-1/src/problem8.ts
+++ Assignment-1/src/problem8.ts
@@ -1,24 +1,19 @@
 {
-  interface T {
+  interface User {
     name: string;
     age: number;
     email: string;
   }
-  
-  type ArrString<T> = {
-    [key in keyof T]: T[key];
-  };
-  
-  function validateKeys(obj: T, keys: (keyof T)[]): boolean {
+
+  function validateKeys(obj: User, keys: (keyof User)[]): boolean {
     let flagToValidateKey = true;
-  
+
     keys.forEach((key) => {
       if (!(key in obj)) flagToValidateKey = false;
     });
     return flagToValidateKey;
   }
-  
-  // const person = { name: "Alice", age: 25, email: "alice@example.com" };
-  // console.log(validateKeys(person, ["name", "age"]));
-  
+
+  const person = { name: "Alice", age: 25, email: "alice@example.com" };
+  console.log(validateKeys(person, ["name", "age"]));
 }
